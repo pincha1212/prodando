a758d1a8f68c5027e00fa42c630581be5d6876bf
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-05-11 16:45:51 -0300

    unos cambios

--- deploy/src/app/app-routing.module.ts
+++ deploy/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
